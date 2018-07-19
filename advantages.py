b85981e7fa85d470d0f0a28943aefec62ca565a8
ANDERSON,WILLIAM (K-ColSprings,ex1) <william.anderson@keysight.com>
2018-07-19 17:05:35 -0600

    Advantage update - still a lot to go

--- advantages.py
+++ advantages.py
@@ -45,6 +45,28 @@ class Advantage:
     def no_rank_cost(self):
         return 1
 
+    def init_flat(self,rank):
+        self.advantage_cost = rank
+        self.advantage_cost_type = Cost_Type.FLAT_RANK
+        self.advantage_rank = rank
+        self.advantage_func_list = None
+        self.calculate_cost = self.flat_cost
+
+    def init_no(self):
+        self.advantage_cost = 1
+        self.advantage_cost_type = Cost_Type.NO_RANK
+        self.advantage_rank = 1
+        self.advantage_func_list = None
+        self.calculate_cost = self.no_rank_cost
+
+    def init_pyramid(self,rank):
+        self.advantage_cost = 0
+        self.advantage_cost_type = Cost_Type.PYRAMID_RANK
+        self.advantage_rank = rank
+        self.advantage_func_list = None
+        self.calculate_cost = self.pyramid_cost
+
+
 
 class Accurate_Attack(Advantage):
     """When you make an accurate attack (see Maneuvers, page
@@ -54,12 +76,217 @@ class Accurate_Attack(Advantage):
 
     def __init__(self, rank):
         super().__init__("Accurate Attack")
-        self.advantage_cost = 0
-        self.advantage_cost_type = Cost_Type.PYRAMID_RANK
-        self.advantage_rank = rank
-        self.advantage_func_list = None
-        self.calculate_cost = self.pyramid_cost
+        self.init_pyramid(rank)
 
+class Agile_Feint(Advantage):
+    """You can use your Acrobatics bonus or movement speed
+    rank in place of Deception to feint and trick in combat as if
+    your skill bonus or speed rank were your Deception bonus
+    (see the Deception skill description). Your opponent opposes
+    the attempt with Acrobatics or Insight (whichever
+    is better)."""
+    def __init__(self):
+        super().__init__("Agile Feint")
+        self.init_no()
+
+class All_Out_Attack(Advantage):
+    """When you make an all-out attack (see Maneuvers, page
+249) you can take a penalty of up to –5 on your active defenses
+(Dodge and Parry) and add the same number (up
+to +5) to your attack bonus."""
+    def __init__(self, rank):
+        super().__init__("All Out Attack")
+        self.init_pyramid(rank)
+
+class Animal_Empathy(Advantage):
+    """You have a special connection with animals. You can use
+interaction skills on animals normally, and do not have to
+speak a language the animal understands; you communicate
+your intent through gestures and body language
+and learn things by studying animal behavior. Characters
+normally have a –10 circumstance penalty to use interaction
+skills on animals, due to their Intellect and lack of language."""
+    def __init__(self):
+        super().__init__("Animal Empathy")
+        self.init_no()
+
+class Artificer(Advantage):
+    """You can use the Expertise: Magic skill to create temporary
+magical devices. See Magical Inventions, page 212, for
+details"""
+    def __init__(self):
+        super().__init__("Artificer")
+        self.init_no()
+
+
+class Assessment(Advantage):
+    """You’re able to quickly size up an opponent’s combat capabilities.
+Choose a target you can accurately perceive and
+have the GM make a secret Insight check for you as a free
+action, opposed by the target’s Deception check result.
+If you win, the GM tells you the target’s attack and defense
+bonuses relative to yours (lower, higher, or equal). With
+each additional degree of success, you learn one of the
+target’s bonuses exactly.
+If you lose the opposed roll, you don’t find out anything.
+With more than one degree of failure, the GM may lie or
+otherwise exaggerate the target’s bonuses."""
+    def __init__(self):
+        super().__init__("Assessment")
+        self.init_no()
+
+class Attractive(Advantage):
+    """You’re particularly attractive, giving you a +2 circumstance
+bonus on Deception and Persuasion checks to deceive,
+seduce, or change the attitude of anyone who finds your
+looks appealing. With a second rank, you are Very Attractive,
+giving you a +5 circumstance bonus. This bonus does
+not count as part of your regular skill bonus in terms of
+the series power level, but also does not apply to people
+or situations which (in the GM’s opinion) would not be influenced
+by your appearance.
+While superheroes tend to be a fairly good-looking lot,
+this advantage is generally reserved for characters with
+particularly impressive looks."""
+    def __init__(self, rank):
+        super().__init__("Attractive")
+        self.init_flat(rank)
+
+class Beginners_Luck(Advantage):
+    """By spending a hero point, you gain an effective 5 ranks in
+one skill of your choice you currently have at 4 or fewer
+ranks, including skills you have no ranks in, even if they can’t
+be used untrained. These temporary skill ranks last for the
+duration of the scene and grant you their normal benefits."""
+    def __init__(self):
+        super().__init__("Beginner's Luck")
+        self.init_no()
+
+class Benefit(Advantage):
+    """You have some significant perquisite or fringe benefit. The
+exact nature of the benefit is for you and the Gamemaster
+to determine. As a rule of thumb it should not exceed the
+benefits of any other advantage, or a power effect costing
+1 point (see Feature in the Powers chapter). It should also
+be significant enough to cost at least 1 power point. An
+example is Diplomatic Immunity (see Sample Benefits).
+A license to practice law or medicine, on the other hand,
+should not be considered a Benefit; it’s simply a part of
+having training in the appropriate Expertise skill and has
+no significant game effect.
+Benefits may come in ranks for improved levels of the
+same benefit. The GM is the final arbiter as to what does
+and does not constitute a Benefit in the setting. Keep in
+mind some qualities may constitute Benefits in some series,
+but not in others, depending on whether or not they
+have any real impact on the game.
+Sample Benefits
+The following are some potential Benefits. The GM is free
+to choose any suitable Benefit for the series.
+• Alternate Identity: You have an alternate identity,
+complete with legal paperwork (driver’s license, birth
+certificate, etc.). This is different from a costumed
+identity, which doesn’t necessarily have any special
+legal status (but may in some settings).
+• Ambidexterity: You are equally adept using either
+hand, suffering no circumstance penalty for using
+your off-hand (as you don’t have one).
+• Cipher: Your true history is well hidden, making it
+difficult to dig up information about you. Investigation
+checks concerning you are made at a –5 circumstance
+penalty per rank in this benefit.
+• Diplomatic Immunity: By dint of your diplomatic
+status, you cannot be prosecuted for crimes in nations
+other than your own. All another nation can do
+is deport you to your home nation.
+• Security Clearance: You have access to classified
+government information, installations, and possibly
+equipment and personnel.
+• Status: By virtue of birth or achievement, you have
+special status. Examples include nobility, knighthood,
+aristocracy, and so forth.
+• Wealth: You have greater than average wealth or
+material resources, such as well-off (rank 1), independently
+wealthy (rank 2), a millionaire (rank 3), multimillionaire
+(rank 4), or billionaire (rank 5)."""
+    def __init__(self, benefit_name, rank):
+        super().__init__("Benefit")
+        self.benefit_name = benefit_name
+        self.init_flat(rank)
+
+class Chokehold(Advantage):
+    """If you successfully grab and restrain an opponent (see
+Grab, page 248), you can apply a chokehold, causing your
+opponent to begin suffocating for as long as you continue
+to restrain your target (see Suffocation, page 238)."""
+    def __init__(self):
+        super().__init__("Chokehold")
+        self.init_no()
+
+class Close_Attack(Advantage):
+    """You have a +1 bonus to close attacks checks per rank in
+this advantage. Your total attack bonus is still limited by
+power level. This advantage best suits characters with a
+level of overall close combat skill (armed and unarmed).
+For capability with a particular type of attack, use the
+Close Combat skill."""
+    def __init__(self, rank):
+        super().__init__("Close Attack")
+        self.init_flat(rank)
+
+class Connected(Advantage):
+    """You know people who can help you out from time to time.
+It might be advice, information, help with a legal matter, or
+access to resources. You can call in such favors by making a
+Persuasion check. The GM sets the DC of the check, based
+on the aid required. A simple favor is DC 10, ranging up to
+DC 25 or higher for especially difficult, dangerous, or expensive
+favors. You can spend a hero point to automatically
+secure the favor, if the GM allows it. The GM has the right to
+veto any request if it is too involved or likely to spoil the plot
+of the adventure. Use of this advantage always requires at
+least a few minutes (and often much longer) and the means
+to contact your allies to ask for their help."""
+    def __init__(self):
+        super().__init__("Connected")
+        self.init_no()
+
+class Contacts(Advantage):
+    """You have such extensive and well-informed contacts you
+can make an Investigation check to gather information
+in only one minute, assuming you have some means of
+getting in touch with your contacts. Further Investigation
+checks to gather information on the same subject require
+the normal length of time, since you must go beyond your
+immediate network of contacts."""
+    def __init__(self):
+        super().__init__("Contacts")
+        self.init_no()
+
+class Daze(Advantage):
+    """You can make a Deception or Intimidation check as a
+standard action (choose which skill when you acquire the
+advantage) to cause an opponent to hesitate in combat.
+Make a skill check as a standard action against your target’s
+resistance check (the same skill, Insight, or Will defense,
+whichever has the highest bonus). If you win, your
+target is dazed (able to take only a standard action) until
+the end of your next round. The ability to Daze with Deception
+and with Intimidation are separate advantages.
+Take this advantage twice in order to be able to do both."""
+    def __init__(self, skill_list):
+        super().__init__("Daze")
+        rank = len(skill_list)
+        self.init_flat(rank)
+
+class Defensive_Attack(Advantage)
+    """When you make a defensive attack (see Maneuvers, page
+249), you can take a penalty of up to –5 on your attack
+bonus and add the same number (up to +5) to both your
+active defenses (Dodge and Parry)."""
+    def __init__(self, rank):
+        super().__init__("Defensive Attack")
+        self.init_pyramid(rank)
 
 
 
