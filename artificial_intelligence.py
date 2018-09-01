3cd130b9a60ca9e3f4a248a80500664153d3a449
ANDERSON,WILLIAM (K-ColSprings,ex1) <william.anderson@keysight.com>
2018-09-01 11:56:44 -0600

    AI proceeding

--- artificial_intelligence.py
+++ artificial_intelligence.py
@@ -6,12 +6,22 @@ class Artificial_Intelligence:
         self.chara = character
         self.env = environment
         self.factions = []
-        self.factums = {}
+        self.objectives = []
+        self.factums = []
 
 class Faction:
     def __init__(self, faction_modifiers):
-        pass
+        self.name = faction_modifiers['Name']
+        self.hostile = []
+
+    def set_hostile(self, faction):
+        self.hostile.append(faction)
 
 class Objective:
     def __init__(self, objective_modifiers):
-        pass
+        self.objective_weight = objective_modiifers['Weight']
+        self.objective_type = objective_modifiers['Type']
+
+
+if __name__ == "__main__":
+    print("Testing")
